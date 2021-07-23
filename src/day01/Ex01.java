ed508ce0b55e0a1fbf41fefbd9a1651e1b591c34
seohee <zawadi0805@gmail.com>
2021-07-23 21:38:02 +0900

    print 기본실행

--- src/day01/Ex01.java
+++ src/day01/Ex01.java
@@ -1,8 +1,16 @@
 package day01;
 
 public class Ex01 {
-	
 	public static void main(String[] args) {
-	
-}
+		System.out.println("안녕하세요");
+		System.out.println("자바 시간입니다.");
+		
+		//println은 아래줄로 내려가서 작성되고, print는 이어서 작성 됨.
+		System.out.print("안녕하세요");
+		System.out.print("자바 시간입니다.");
+		
+		
+		
+	}
+
 }
