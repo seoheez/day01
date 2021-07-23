2f1dda9559c25845d8ba14b6589139a1c89b1333
seohee <zawadi0805@gmail.com>
2021-07-23 21:39:59 +0900

    주석 방법

--- src/day01/Ex01.java
+++ src/day01/Ex01.java
@@ -9,7 +9,12 @@ public class Ex01 {
 		System.out.print("안녕하세요");
 		System.out.print("자바 시간입니다.");
 		
-		
+		  /*
+		   * sysout ctrl + spacebar는 괄호 안의 내용을 출력하는 기능
+		   * ;(쎄미콜론) 은 문장의 끝을 알리는 용도
+		  */
+		 
+	
 		
 	}
 
